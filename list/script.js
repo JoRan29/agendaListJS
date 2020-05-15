9fc9908b98a79735df70ad3fd48b632f4337e11b
JoRan29 <jokavana29@gmail.com>
2020-05-15 13:38:13 +0200

    moved modal to other folder

--- list/script.js
+++ list/script.js
@@ -64,34 +64,3 @@ inputNewTask.addEventListener("keyup", (e) => {
     inputNewTask.value = "";
   }
 });
-
-// Modal
-// let openModalButtons = document.querySelectorAll("[data-modal-target]");
-// let closeModalButtons = document.querySelectorAll("[data-close-button]");
-// let overlay = document.getElementById("overlay");
-
-// openModalButtons.forEach((b) => {
-//   b.addEventListener("click", () => {
-//     let modal = document.querySelector(button.dataset.modalTarget);
-//     openModal(modal);
-//   });
-// });
-
-// closeModalButtons.forEach((b) => {
-//   b.addEventListener("click", () => {
-//     let modal = button.closest(".modal");
-//     closeModal(modal);
-//   });
-// });
-
-// let openModal = () => {
-//   if (modal == null) return;
-//   modal.classList.add("active");
-//   overlay.classList.add("active");
-// };
-
-// let closeModal = () => {
-//   if (modal == null) return;
-//   modal.classList.remove("active");
-//   overlay.classList.remove("active");
-// };
